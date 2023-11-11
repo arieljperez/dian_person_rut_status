5934336865c5aeae7e8b15b31393b1a1613fe4c5
arielperezti <ariel.perez.ti@gmail.com>
2023-11-11 13:11:19 -0500

    customs module imporation doesn't want to work, so i'm trying to improve its importation

--- dian_person_rut_status/muisca.py
+++ dian_person_rut_status/muisca.py
@@ -6,9 +6,9 @@ from requests.models import Request
 
 from bs4 import BeautifulSoup
 
-from custom.exceptions import TokenException
-from custom.types import RutStatusDict
-from custom import utils
+from .custom.exceptions import TokenException
+from .custom.types import RutStatusDict
+from .custom import utils
 
 
   
