50bd828df89a162183b96ea1c90b9813589d0748
arielperezti <ariel.perez.ti@gmail.com>
2023-11-11 01:56:21 -0500

    added custom exceptions and types folder and some validations into get_person_info introducing it as a method inside a DianRut class

---
+++ core/custom/types.py
@@ -0,0 +1,7 @@
+from typing import TypedDict
+
+class PersonDataDict(TypedDict):
+    nit:str
+    name:str
+    dv:str
+    status:str
--- core/main.py
+++ core/main.py
@@ -1,8 +1,14 @@
 from typing import Union, Dict
 
 import requests
+
+from requests.models import Request
+
 from bs4 import BeautifulSoup
 from bs4.element import Tag
+
+from custom.exceptions import TokenException
+from custom.types import PersonDataDict
 
 import os
 
@@ -11,8 +17,6 @@ FORM_CLASS_INFO = '.tipoFilaNormalVerde'
 TOKEN_FIELD = 'com.sun.faces.VIEW'
 
 FORM_PREFIX = 'vistaConsultaEstadoRUT:formConsultaEstadoRUT'
-
-TOKEN = os.environ.get('TOKEN')
 
 VALUE_ATTR = 'value'
 
@@ -44,24 +48,23 @@ def get_value_from_tag(tag:Tag, value:str)->Union[str, None]:
 
 
 def find_value_in_soup(soup:BeautifulSoup, value:str)->Union[str, None]:
-    if not (tag := soup.find(attrs={'id': as_form_field(value)})):
-        return
     
-    return get_value_from_tag(tag=tag, value=VALUE_ATTR)
+    tag = soup.find(attrs={'id': as_form_field(value)})
+    
+    return None if not tag else get_value_from_tag(tag=tag, value=VALUE_ATTR)
 
 def concat_full_name_by_name_strings(names:list)->Union[str, None]:
     return ' '.join(names) if all(names) else None
 
 
-def get_person_name_from_soup(soup:BeautifulSoup):
+def get_person_name_from_soup(soup:BeautifulSoup)->Union[str, None]:
     
-    if name := find_value_in_soup(soup=soup, value='razonSocial'):
-        return name
+    name = find_value_in_soup(soup=soup, value='razonSocial')
     
-    return concat_full_name_by_name_strings([find_value_in_soup(soup=soup, value=value) for value in NATURAL_PERSON_VALUES])
+    return name if name else concat_full_name_by_name_strings([find_value_in_soup(soup=soup, value=value) for value in NATURAL_PERSON_VALUES])
 
     
-def get_person_data_from_soup(soup:BeautifulSoup):
+def get_person_data_from_soup(soup:BeautifulSoup)->Union[PersonDataDict, None]:
     
     person_data = {PERSON_VALUES_KEYS[0]: find_value_in_soup(soup=soup, value='numNit'),
                      PERSON_VALUES_KEYS[1]: find_value_in_soup(soup=soup, value='dv'),
@@ -80,26 +83,36 @@ PAYLOAD_DATA = {as_form_field('modoPresentacionSeleccionBO'): 'pantalla',
                 as_form_field('mantenerCriterios'): '',
                 as_form_field('btnBuscar.x'): 48,
                 as_form_field('btnBuscar.y'): 11,
-                TOKEN_FIELD: TOKEN,
                 as_form_field(): 'vistaConsultaEstadoRUT:formConsultaEstadoRUT',
                 as_form_field('_idcl'): '',
                 }
 
 
-
-def get_person_info(nit:str, attempts:int=DEFAULT_ATTEMPTS, timeout:int=DEFAULT_TIMEOUT)->Union[Dict, None]:
+class DianRut:
     
-    if attempts == 0:
-        return None
-    
-    try:
-        response = requests.post(url=WEB_RUT_MUISCA_URL, data={**PAYLOAD_DATA, as_form_field('numNit'): nit}, timeout=timeout)
-    except (Exception, requests.Timeout) as possible_errors:
-        return get_person_info(nit=nit, attempts=DEFAULT_ATTEMPTS-1)
-    
-    soup = BeautifulSoup(response.text, features='html.parser') if response else None
-    
-    if not soup:
-        return
-    
-    return get_person_data_from_soup(soup=soup)
+    @staticmethod
+    def get_person_info(token:str, nit:str, attempts:int=DEFAULT_ATTEMPTS, timeout:int=DEFAULT_TIMEOUT)->Union[PersonDataDict, None]:
+        
+        if not token:
+            raise TokenException('The Token is Missing!')
+        
+        if attempts == 0:
+            print('All attempts completed! ')
+            return
+        
+        try:
+            response = requests.post(url=WEB_RUT_MUISCA_URL, data={**PAYLOAD_DATA, as_form_field('numNit'): nit, TOKEN_FIELD: token}, timeout=timeout)
+            
+            if isinstance(response, Request):
+                response.raise_for_status()
+                
+        except requests.Timeout:
+            return DianRut.get_person_info(token=token, nit=nit, attempts=DEFAULT_ATTEMPTS-1)
+        except Exception as e:
+            print(e)
+        else:
+            soup = BeautifulSoup(response.text, features='html.parser') if response else None
+            
+            return None if not soup else get_person_data_from_soup(soup=soup)
+        
+#  token = os.environ.get('TOKEN')
